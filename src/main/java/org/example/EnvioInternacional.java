410473869f0b501176ac18f489d29ae92f5a7185
MaKz51 <maxyracer@gmail.com>
2026-08-02 03:21:26 -0600

    avance

--- src/main/java/org/example/EnvioInternacional.java
+++ src/main/java/org/example/EnvioInternacional.java
@@ -1,4 +1,25 @@
 package org.example;
 
-public class EnvioInternacional {
+public class EnvioInternacional implements EstrategiaEnvio {
+    private boolean cobroAduanaAplicado;
+
+    public EnvioInternacional() {
+        this.cobroAduanaAplicado = false;
+    }
+
+    @Override
+    public double calcularCosto(double pesoKg){
+        double costoBase = pesoKg *6.00;
+
+        if(cobroAduanaAplicado){
+            costoBase += 10.00;
+            this.cobroAduanaAplicado = true;
+        }
+        return costoBase;
+    }
+    @Override
+    public String obtenerTipoEnvio() {
+        return "Envio Internacional";
+    }
+
 }
