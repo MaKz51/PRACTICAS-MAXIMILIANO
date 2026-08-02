493d2b75b39127ee38bab5dc7ef5868d5ca77eed
MaKz51 <maxyracer@gmail.com>
2026-08-01 18:10:16 -0600

    logicas en las clases hechas

--- src/main/java/org/example/Estacionamiento.java
+++ src/main/java/org/example/Estacionamiento.java
@@ -1,4 +1,61 @@
 package org.example;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class Estacionamiento {
+    private ArrayList<Vehiculo> vehiculos = new ArrayList();
+
+    public Estacionamiento() {
+    }
+
+    public void registrarVehiculo(Vehiculo vehiculo) throws Exception {
+        String sql = "INSERT INTO vehiculos (placa, tipo, horas) VALUES (?, ?, ?)";
+        PreparedStatement comando = Main.conexion.prepareStatement(sql);
+        comando.setString(1, vehiculo.getPlaca());
+
+        comando.setString(2, vehiculo.getClass().getSimpleName());
+        comando.setInt(3, vehiculo.getHorasEstacionado());
+
+        comando.executeUpdate();
+        System.out.println("Vehiculo registrado en la base de datos");
+    }
+
+    public void imprimirReporte() throws Exception {
+        double total = 0;
+        System.out.println("===== ESTACIONAMIENTO GALERIAS =====");
+
+        String sql = "SELECT * FROM vehiculos";
+        PreparedStatement comando = Main.conexion.prepareStatement(sql);
+        ResultSet resultado = comando.executeQuery();
+
+        while (resultado.next()) {
+            String placa = resultado.getString("placa");
+            String tipo = resultado.getString("tipo");
+            int horas = resultado.getInt("horas");
+
+            Vehiculo v = null;
+            if (tipo.equalsIgnoreCase("Auto")) {
+                v = new Auto(placa, horas);
+            } else if (tipo.equalsIgnoreCase("Moto")) {
+                v = new Moto(placa, horas);
+            } else if (tipo.equalsIgnoreCase("Camion")) {
+                v = new Camion(placa, horas);
+            }
+
+            if (v != null) {
+                double costo = v.calcularCostoEstacionamiento();
+                System.out.println("-----------------------------------");
+                System.out.println("Tipo: ");
+                System.out.println("Placa: " + v.getPlaca());
+                System.out.println("Horas: " + v.getHorasEstacionado());
+                System.out.println("Costo: $" + costo);
+                total += costo;
+            }
+        }
+
+        System.out.println("-----------------------------------");
+        System.out.println("TOTAL: $" + total);
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Scanner;
@@ -7,10 +8,65 @@ public class Main {
     static Estacionamiento estacionamiento = new Estacionamiento();
     static Scanner teclado = new Scanner(System.in);
 
-    static String url = "jdbc:sqlserver://localhost\\SQLEXPRESS;database=ESTACIONAMIENTO_POO";
+    static String url = "jdbc:sqlserver://localhost\\SQLEXPRESS;database=ESTACIONAMIENTO_POO;user=sa;password=Maxmi2017Th;encrypt=true;trustServerCertificate=true;";
+    public static Connection conexion;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+    conexion = DriverManager.getConnection(url);
+    int opcion = 0;
+
+    do {
+        menu();
+        opcion = teclado.nextInt();
+        teclado.nextLine();
+
+        switch (opcion) {
+            case 1:
+                nuevoVehiculo();
+                break;
+            case 2:
+                estacionamiento.imprimirReporte();
+                break;
+            case 3:
+                System.out.println("Bye Bye.");
+                break;
+            default:
+                System.out.println("Opción no válida");
+                break;
+        }
+    }while (opcion != 3);
+
+    conexion.close();
+
+    }
 
 
+    public static void nuevoVehiculo() throws Exception {
+        System.out.println("Escriba el tipo de vehículo.");
+        System.out.println("- Auto");
+        System.out.println("- Moto");
+        System.out.println("- Camion");
+        String opcionVehiculo = teclado.next();
+        System.out.println("Escriba las placas.");
+        String placasVehiculos = teclado.next();
+        System.out.println("Escriba las horas estacionado.");
+        int horitas = teclado.nextInt();
+        if (opcionVehiculo.equalsIgnoreCase("Auto")) {
+            estacionamiento.registrarVehiculo(new Auto(placasVehiculos, horitas));
+        } else if (opcionVehiculo.equalsIgnoreCase("Moto")) {
+            estacionamiento.registrarVehiculo(new Moto(placasVehiculos, horitas));
+        } else if (opcionVehiculo.equalsIgnoreCase("Camion")) {
+            estacionamiento.registrarVehiculo(new Camion(placasVehiculos, horitas));
+        } else {
+            System.out.println("Tipo de vehículo no válido.");
+        }
+
+    }
+
+    public static void menu() {
+        System.out.println("======================MENÚ======================");
+        System.out.println("1) Registrar nuevo vehiculo");
+        System.out.println("2) Imprimir Reporte");
+        System.out.println("3) Salir");
     }
 }
